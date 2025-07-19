25a8cbd277081d5f90e69d81b177bff2703a3eb4
Ahsanul Haque Nehal <007cryptic@gmail.com>
2025-07-19 20:55:27 +0600

    project complete

--- src/features/themeToggle.jsx
+++ src/features/themeToggle.jsx
@@ -12,7 +12,7 @@ const ThemeToggle = () => {
       className="w-10 h-10 flex items-center justify-center rounded-full transition-colors duration-300 hover:bg-orange-500 dark:hover:bg-orange-300 text-white bg-white "
     >
       {isDark ? (
-        <FaSun className="text-yellow-400" />
+        <FaSun className="text-[#FF8901]" />
       ) : (
         <FaMoon className="text-blue-400" />
       )}
